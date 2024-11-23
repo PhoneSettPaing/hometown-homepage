df8b0c1ed807ab0e047c5ca6a834f16eebec03de
Phone Sett Paing <phonesettpaing1995@gmail.com>
2024-11-23 05:15:24 +0000

    Added button and  script.js file

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('btn_palace').addEventListener('click', (event) => {
+    window.open("https://en.wikipedia.org/wiki/Mandalay_Palace", "_blank");
+  
+});
+
+document.getElementById('btn_bridge').addEventListener('click', (event) => {
+    window.open("https://en.wikipedia.org/wiki/U_Bein_Bridge", "_blank");
+  
+});
+
+document.getElementById('btn_hill').addEventListener('click', (event) => {
+    window.open("https://en.wikipedia.org/wiki/Mandalay_Hill", "_blank");
+  
+});
+
+document.getElementById('btn_pagoda').addEventListener('click', (event) => {
+    window.open("https://en.wikipedia.org/wiki/Kuthodaw_Pagoda", "_blank");
+  
+});
